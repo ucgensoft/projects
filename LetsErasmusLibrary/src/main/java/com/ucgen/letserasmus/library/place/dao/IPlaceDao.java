dcf4250432b7766ff3308915a2fb047f41763413
keagler <TTKGULER@TC08685508.turkcell.entp.tgc>
2016-12-05 22:57:16 +0300

    KG

--- LetsErasmusLibrary/src/main/java/com/ucgen/letserasmus/library/place/dao/IPlaceDao.java
+++ LetsErasmusLibrary/src/main/java/com/ucgen/letserasmus/library/place/dao/IPlaceDao.java
@@ -1,5 +1,13 @@
 package com.ucgen.letserasmus.library.place.dao;
 
+import com.ucgen.common.operationresult.ListOperationResult;
+import com.ucgen.common.operationresult.ValueOperationResult;
+import com.ucgen.letserasmus.library.place.model.Place;
+
 public interface IPlaceDao {
+	
+	ListOperationResult<Place> getPlace(Long id);
+	ValueOperationResult<Integer> insertPlace(Place place);
+	ValueOperationResult<Integer> updatePlace(Place place);
 
 }
