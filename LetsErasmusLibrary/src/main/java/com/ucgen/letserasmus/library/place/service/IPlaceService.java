c1d916ffa5a9aaf8f9ba5b5ccfaa49a2252f5216
keagler <TTKGULER@TC08685508.turkcell.entp.tgc>
2016-12-05 23:01:06 +0300

    KG

--- LetsErasmusLibrary/src/main/java/com/ucgen/letserasmus/library/place/service/IPlaceService.java
+++ LetsErasmusLibrary/src/main/java/com/ucgen/letserasmus/library/place/service/IPlaceService.java
@@ -1,5 +1,13 @@
 package com.ucgen.letserasmus.library.place.service;
 
+import com.ucgen.common.operationresult.ListOperationResult;
+import com.ucgen.common.operationresult.ValueOperationResult;
+import com.ucgen.letserasmus.library.place.model.Place;
+
 public interface IPlaceService {
+	
+	ListOperationResult<Place> getPlace(Long id);
+	ValueOperationResult<Integer> insertPlace(Place place);
+	ValueOperationResult<Integer> updatePlace(Place place);
 
 }
--- LetsErasmusLibrary/src/main/java/com/ucgen/letserasmus/library/place/service/impl/PlaceService.java
+++ LetsErasmusLibrary/src/main/java/com/ucgen/letserasmus/library/place/service/impl/PlaceService.java
@@ -1,5 +1,41 @@
 package com.ucgen.letserasmus.library.place.service.impl;
 
-public class PlaceService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ucgen.common.operationresult.ListOperationResult;
+import com.ucgen.common.operationresult.ValueOperationResult;
+import com.ucgen.letserasmus.library.place.dao.IPlaceDao;
+import com.ucgen.letserasmus.library.place.model.Place;
+import com.ucgen.letserasmus.library.place.service.IPlaceService;
+
+@Service
+public class PlaceService implements IPlaceService{
+
+	private IPlaceDao iPlaceDao;
+	
+	public IPlaceDao getiPlaceDao() {
+		return iPlaceDao;
+	}
+
+	@Autowired
+	public void setiPlaceDao(IPlaceDao iPlaceDao) {
+		this.iPlaceDao = iPlaceDao;
+	}
+
+	@Override
+	public ListOperationResult<Place> getPlace(Long id) {
+		return this.getiPlaceDao().getPlace(id);
+	}
+
+	@Override
+	public ValueOperationResult<Integer> insertPlace(Place place) {
+		return this.getiPlaceDao().insertPlace(place);
+	}
+
+	@Override
+	public ValueOperationResult<Integer> updatePlace(Place place) {
+		return this.getiPlaceDao().updatePlace(place);
+	}
 
 }
